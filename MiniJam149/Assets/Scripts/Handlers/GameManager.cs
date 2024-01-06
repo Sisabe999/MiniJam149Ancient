75b128a155c431e827bf4c2ddc580a82a00b4f8c
Miguel Hoyos <mangelhoyos101@gmail.com>
2024-01-06 12:03:53 -0500

    Game manager, UI Manager and Main Menu Scene, Sound effects added

--- MiniJam149/Assets/Scripts/Handlers/GameManager.cs
+++ MiniJam149/Assets/Scripts/Handlers/GameManager.cs
@@ -1,38 +1,86 @@
+using Mono.Cecil;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+
+    public enum GameStatus
+    {
+        RUNNING,
+        CONSOLE,
+        GAMEOVER
+    }
+
+    public GameStatus actualStatus { get; private set; }
+
     [Header("Dependencies")]
     [SerializeField] private UIManager uiManager;
+    [SerializeField] private ActivityHandler activityHandler;
 
     private int points;
     private int timeLine;
+    private const int TIMELINELIMIT = 5;
     private float gameSpeed;
+    private int gamesWonInARow = 0;
 
-    private bool isInGame = false;
-    private bool gameEnded = false;
+    public void Start()
+    {
+        if(Instance != null)
+        {
+            Destroy(Instance.gameObject);
+            Instance = this;
+        }
+        else
+        {
+            Instance = this;
+        }
+        timeLine = TIMELINELIMIT - 1;
+    }
+
+    public void SetGameStatus(GameStatus newStatus)
+    {
+        actualStatus = newStatus;
+    }
 
     public void SendActivityState(bool isFailed)
     {
-        uiManager.SetUI(timeLine);
+        if(isFailed)
+        {
+            timeLine -= 1;
+            if(timeLine <= 0)
+                GameOver();
+        }
+        else
+        {
+            gamesWonInARow++;
+            if(gamesWonInARow >= 2)
+            {
+                timeLine += 1;
+                gamesWonInARow = 0;
+            }
+            gameSpeed += 0.35f;
+            SetGameStatus(GameStatus.RUNNING);
+        }
+        uiManager.SetUI(timeLine, points);
     }
 
     public void StartActivity()
     {
-        if (isInGame)
+        if (actualStatus != GameStatus.RUNNING)
             return;
 
-        uiManager.SetConsoleVisibility(true);
-        isInGame = true;
+        uiManager.SetConsoleVisibility(true, () => activityHandler.StartGame(gameSpeed));
+        SetGameStatus(GameStatus.CONSOLE);
     }
 
     public void GameOver()
     {
-        if(gameEnded) 
-            return;
-
-        uiManager.ShowGameOver();
-        gameEnded = true;
+        if (actualStatus == GameStatus.RUNNING || actualStatus == GameStatus.CONSOLE)
+        {
+            uiManager.ShowGameOver();
+            SetGameStatus(GameStatus.GAMEOVER);
+        }
     }
 
 }
